defd4f60e15e8a58e235fa47cdf4ab6fa8ac30e9
minghang <minghang@staff.weibo.com>
2024-10-18 16:40:10 +0800

    自动安装docker-ce

--- begin.sh
+++ begin.sh
@@ -16,19 +16,21 @@ sed -i '$a\\' sources.list.d/armbian.list
 sed -i '$a\deb [signed-by=/usr/share/keyrings/armbian.gpg] https://mirrors.ustc.edu.cn/armbian bookworm main bookworm-utils bookworm-desktop' sources.list.d/armbian.list
 
 apt update
+apt upgrade -y
 
 apt install -y ca-certificates curl software-properties-common
 
 #curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | apt-key add -
-#mv trusted.gpg trusted.gpg.d/docker-ce.gpg
-#add-apt-repository "deb [arch=arm64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
+#mv trusted.gpg /etc/apt/trusted.gpg.d/docker-ce.gpg
+#add-apt-repository -y "deb [arch=arm64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
 
-#curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | gpg --import -
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | gpg --import -
 #gpg --list-keys --with-colons #fpr字段为指纹
 #gpg --list-keys #key字段后面为id
-#gpg --output /etc/apt/trusted.gpg.d/docker-ce.gpg --export $KEYID_OR_FINGERPRINT
-#add-apt-repository "deb [arch=arm64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
+KEYID_OR_FINGERPRINT=$(gpg --list-keys | sed -n '/Docker/{g;p;};h')
+#KEYID_OR_FINGERPRINT=$(gpg --list-keys | awk '/Docker/ {print prev} {prev=$0}')
+gpg --output /etc/apt/keyrings/docker-ce.gpg --export $KEYID_OR_FINGERPRINT
+echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker-ce.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker-ce.list
 
-curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg > /etc/apt/trusted.gpg.d/docker-ce.asc
-gpg -output /etc/apt/keyrings/docker-ce.gpg
-add-apt-repository 'deb [arch=arm64 signed-by=/etc/apt/keyrings/docker-ce.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/debian '$(lsb_release -cs)' stable'
+apt update
+apt install -y docker-ce
