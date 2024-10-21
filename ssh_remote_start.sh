0a0c427293a9ec21c0c62877f545e015776dc929
minghang <minghang@staff.weibo.com>
2024-10-21 15:46:59 +0800

    添加ssh远程配置服务器脚本

---
+++ ssh_remote_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ip_address=${1}
+ssh root@${ip_address} 'apt install git'
+ssh root@${ip_address} 'git clone https://github.com/zmhhaha/armbianbegin.git /root'
+ssh root@${ip_address} 'bash /root/armbianbegin/debian_begin.sh'
