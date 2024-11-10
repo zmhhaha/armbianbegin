ee6d13e7913c7ddd3c41072821863813c8a7203b
zmhhaha <627955836@qq.com>
2024-11-10 22:24:36 +0800

    修改

--- ssh_config.sh
+++ ssh_config.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-password=${1:-1234}
-ssh-keygen -t rsa -b 2048 -N ""
-touch ~/.ssh/authorized_keys
-sshpass -p ${password} ssh-copy-id -o StrictHostKeyChecking=no nanopct4-master
-sshpass -p ${password} ssh-copy-id -o StrictHostKeyChecking=no nanopct4-server1
-sshpass -p ${password} ssh-copy-id -o StrictHostKeyChecking=no nanopct4-server2
+ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ""
+ssh-copy-id -o StrictHostKeyChecking=no nanopct4-master
+ssh-copy-id -o StrictHostKeyChecking=no nanopct4-server1
+ssh-copy-id -o StrictHostKeyChecking=no nanopct4-server2
