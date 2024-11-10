bdf9f233c55d921ae78660be7a7a982a8e5521ed
zmhhaha <627955836@qq.com>
2024-11-10 14:43:26 +0800

    修改

--- ssh_remote_start.sh
+++ ssh_remote_start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ip_address=${1}
-name_tail=${2}
-static_ip_address=${3}
+option=${2:-restart}
+name_tail=${3}
+static_ip_address=${4}
 ssh root@${ip_address} 'apt install -y git'
 ssh root@${ip_address} 'rm -rf /root/armbianbegin'
 ssh root@${ip_address} 'git clone https://github.com/zmhhaha/armbianbegin.git /root/armbianbegin'
-ssh root@${ip_address} 'bash /root/armbianbegin/debian_begin.sh start '${name_tail}' '${static_ip_address}'
+ssh root@${ip_address} 'bash /root/armbianbegin/debian_begin.sh '${option}' '${name_tail}' '${static_ip_address}'
